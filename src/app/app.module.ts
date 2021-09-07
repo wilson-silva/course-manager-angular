b6edb6f4e979d3c9e6ed75a4a2930169b7801e38
Wilson <wilson-gs@hotmail.com>
2021-09-07 12:46:43 -0300

    Handling Errors with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { RouterModule } from '@angular/router';
 import {  HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component  
   ],
   imports: [
     BrowserModule,
@@ -22,10 +19,8 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
+   
     ])
   ],
   providers: [],
