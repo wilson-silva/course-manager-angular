6e56a6b36dfa720ee88467fcb862d56ac603cb0a
Wilson <wilson-gs@hotmail.com>
2021-09-07 12:25:33 -0300

    Creating the core folder

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { NavBarComponent } from "./component/nav-bar/nav-bar.component";
+
+
+
+@NgModule({
+    declarations: [
+        NavBarComponent
+    ],
+    imports:[
+        RouterModule
+    ],
+    exports:[
+        NavBarComponent
+    ]
+})
+export class CoreModule{
+
+
+}
